4ea654f64b0f8f609f0fdd4e191a8fa0c523e976
elinpelinfelin <elinstaaf@gmail.com>
2023-12-12 08:29:30 +0100

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,32 +3,6 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-import os
-from trubrics import Trubrics
-
-trubrics = Trubrics(
-    project="default",
-    email=os.environ["TRUBRICS_EMAIL"],
-    password=os.environ["TRUBRICS_PASSWORD"],
-)
-
-user_prompt = trubrics.log_prompt(
-    config_model={"model": "gpt-3.5-turbo"},
-    prompt="Tell me a joke",
-    generation="Why did the chicken cross the road? To get to the other side.",
-)
-
-user_feedback = trubrics.log_feedback(
-    component="default",
-    model=user_prompt.config_model.model,
-    prompt_id=user_prompt.id,
-    user_response={
-        "type": "thumbs",
-        "score": "👎",
-        "text": "Not a very funny joke...",
-    }
-)
-
 """
 # Welcome to Streamlit!
 
