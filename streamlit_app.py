b6ff1f6eb43080fa51e2af6b70effc31a4f47905
elinpelinfelin <elinstaaf@gmail.com>
2023-12-12 07:29:02 +0100

    ugh

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,34 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+from trubrics.integrations.streamlit import FeedbackCollector
+
+if "logged_prompt" not in st.session_state:
+    st.session_state.logged_prompt = None
+
+collector = FeedbackCollector(
+    email=st.secrets.TRUBRICS_EMAIL,
+    password=st.secrets.TRUBRICS_PASSWORD,
+    project="default"
+)
+
+if st.button("Predict"):
+    st.session_state.logged_prompt = collector.log_prompt(
+        config_model={"model": "gpt-3.5-turbo"},
+        prompt="Tell me a joke",
+        generation="Why did the chicken cross the road? To get to the other side.",
+    )
+
+if st.session_state.logged_prompt:
+    st.write("A model generation...")
+    user_feedback = collector.st_feedback(
+        component="default",
+        feedback_type="thumbs",
+        open_feedback_label="[Optional] Provide additional feedback",
+        model=st.session_state.logged_prompt.config_model.model,
+        prompt_id=st.session_state.logged_prompt.id,
+        align="flex-start"
+    )
 
 """
 # Welcome to Streamlit!
@@ -38,25 +66,3 @@ st.altair_chart(alt.Chart(df, height=700, width=700)
         color=alt.Color("idx", legend=None, scale=alt.Scale()),
         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
     ))
-
-
-import streamlit as st
-from trubrics.integrations.streamlit import FeedbackCollector
-
-collector = FeedbackCollector(
-    email=st.secrets.TRUBRICS_EMAIL,
-    password=st.secrets.TRUBRICS_PASSWORD,
-    project="default"
-)
-
-user_feedback = collector.st_feedback(
-    component="default",
-    feedback_type="thumbs",
-    open_feedback_label="[Optional] Provide additional feedback",
-    model="gpt-3.5-turbo",
-    prompt_id=None,  # checkout collector.log_prompt() to log your user prompts
-)
-
-if user_feedback:
-    st.write("#### Raw feedback saved to Trubrics:")
-    st.write(user_feedback)
