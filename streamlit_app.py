d6d010549bff84b9a2aafc3baccbdf4a7ce2427c
elinpelinfelin <elinstaaf@gmail.com>
2023-12-12 06:35:03 +0100

    test3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,69 +1,31 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+# pip install "trubrics[streamlit]"
+
 import streamlit as st
+from trubrics.integrations.streamlit import FeedbackCollector
 
-"""
-# Welcome to Streamlit!
+if "logged_prompt" not in st.session_state:
+    st.session_state.logged_prompt = None
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
-
-
-# pip install trubrics
-
-import os
-from trubrics import Trubrics
-
-trubrics = Trubrics(
-    project="default",
-    email=os.environ["TRUBRICS_EMAIL"],
-    password=os.environ["TRUBRICS_PASSWORD"],
+collector = FeedbackCollector(
+    email=st.secrets.TRUBRICS_EMAIL,
+    password=st.secrets.TRUBRICS_PASSWORD,
+    project="default"
 )
 
-user_prompt = trubrics.log_prompt(
-    config_model={"model": "gpt-3.5-turbo"},
-    prompt="Tell me a joke",
-    generation="Why did the chicken cross the road? To get to the other side.",
-)
+if st.button("Predict"):
+    st.session_state.logged_prompt = collector.log_prompt(
+        config_model={"model": "gpt-3.5-turbo"},
+        prompt="Tell me a joke",
+        generation="Why did the chicken cross the road? To get to the other side.",
+    )
 
-user_feedback = trubrics.log_feedback(
-    component="default",
-    model=user_prompt.config_model.model,
-    prompt_id=user_prompt.id,
-    user_response={
-        "type": "thumbs",
-        "score": "👎",
-        "text": "Not a very funny joke...",
-    }
-)
+if st.session_state.logged_prompt:
+    st.write("A model generation...")
+    user_feedback = collector.st_feedback(
+        component="default",
+        feedback_type="thumbs",
+        open_feedback_label="[Optional] Provide additional feedback",
+        model=st.session_state.logged_prompt.config_model.model,
+        prompt_id=st.session_state.logged_prompt.id,
+        align="flex-start"
+    )
