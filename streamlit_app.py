3518d243ffcf6b9a151f9ab299a5b0aa04eb0aca
elinpelinfelin <elinstaaf@gmail.com>
2023-12-12 05:54:11 +0100

    test1

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,28 +13,30 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+# pip install trubrics
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+import os
+from trubrics import Trubrics
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+trubrics = Trubrics(
+    project="default",
+    email=os.environ["TRUBRICS_EMAIL"],
+    password=os.environ["TRUBRICS_PASSWORD"],
+)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+user_prompt = trubrics.log_prompt(
+    config_model={"model": "gpt-3.5-turbo"},
+    prompt="Tell me a joke",
+    generation="Why did the chicken cross the road? To get to the other side.",
+)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+user_feedback = trubrics.log_feedback(
+    component="default",
+    model=user_prompt.config_model.model,
+    prompt_id=user_prompt.id,
+    user_response={
+        "type": "thumbs",
+        "score": "👎",
+        "text": "Not a very funny joke...",
+    }
+)
