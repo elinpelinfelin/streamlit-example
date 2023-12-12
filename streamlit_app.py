1dbbd9746b12b93a0ab07ba69fad1d62ab043b81
elinpelinfelin <elinstaaf@gmail.com>
2023-12-12 08:26:07 +0100

    bong

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,34 +2,32 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-from trubrics.integrations.streamlit import FeedbackCollector
 
-if "logged_prompt" not in st.session_state:
-    st.session_state.logged_prompt = None
+import os
+from trubrics import Trubrics
 
-collector = FeedbackCollector(
-    email=st.secrets.TRUBRICS_EMAIL,
-    password=st.secrets.TRUBRICS_PASSWORD,
-    project="default"
+trubrics = Trubrics(
+    project="default",
+    email=os.environ["TRUBRICS_EMAIL"],
+    password=os.environ["TRUBRICS_PASSWORD"],
 )
 
-if st.button("Predict"):
-    st.session_state.logged_prompt = collector.log_prompt(
-        config_model={"model": "gpt-3.5-turbo"},
-        prompt="Tell me a joke",
-        generation="Why did the chicken cross the road? To get to the other side.",
-    )
+user_prompt = trubrics.log_prompt(
+    config_model={"model": "gpt-3.5-turbo"},
+    prompt="Tell me a joke",
+    generation="Why did the chicken cross the road? To get to the other side.",
+)
 
-if st.session_state.logged_prompt:
-    st.write("A model generation...")
-    user_feedback = collector.st_feedback(
-        component="default",
-        feedback_type="thumbs",
-        open_feedback_label="[Optional] Provide additional feedback",
-        model=st.session_state.logged_prompt.config_model.model,
-        prompt_id=st.session_state.logged_prompt.id,
-        align="flex-start"
-    )
+user_feedback = trubrics.log_feedback(
+    component="default",
+    model=user_prompt.config_model.model,
+    prompt_id=user_prompt.id,
+    user_response={
+        "type": "thumbs",
+        "score": "👎",
+        "text": "Not a very funny joke...",
+    }
+)
 
 """
 # Welcome to Streamlit!
